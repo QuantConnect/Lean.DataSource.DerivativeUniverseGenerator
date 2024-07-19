f50c935efbf5195615e7b54d711182ffd0a8a9e9
LouisSzeto <hke0073@icloud.com>
2024-07-19 13:12:02 +0800

    add option iv rank and percentile as additional field

---
+++ Lean.DataSource.OptionsUniverseGenerator/OptionAdditionalFieldGenerator.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2024 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using QuantConnect.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace QuantConnect.DataSource.OptionsUniverseGenerator
+{
+    public class OptionAdditionalFieldGenerator : DerivativeUniverseGenerator.AdditionalFieldGenerator
+    {
+        private const string _impliedVolHeader = "implied_volatility";
+        private const string _deltaHeader = "delta";
+        private const string _sidHeader = "#symbol_id";
+        private const string _tickerHeader = "symbol_value";
+
+        public OptionAdditionalFieldGenerator(DateTime processingDate, string rootPath)
+            : base(processingDate, rootPath)
+        {
+        }
+
+        public override bool Run()
+        {
+            Log.Trace($"OptionAdditionalFieldGenerator.Run(): Processing additional fields for date {_processingDate:yyyy-MM-dd}");
+
+            // per symbol
+            try
+            {
+                foreach (var subFolder in Directory.GetDirectories(_rootPath))
+                {
+                    var dateFile = Path.Combine(subFolder, $"{_processingDate:yyyyMMdd}.csv");
+                    var symbol = subFolder.Split(Path.DirectorySeparatorChar)[^1].ToUpper();
+                    if (!File.Exists(dateFile))
+                    {
+                        Log.Error($"OptionAdditionalFieldGenerator.Run(): no universe file found for {symbol} in {_processingDate:yyyy-MM-dd}");
+                        return false;
+                    }
+
+                    var ivs = GetIvs(_processingDate, subFolder);
+                    var additionalFields = new OptionAdditionalFields();
+                    additionalFields.Update(ivs);
+
+                    WriteToCsv(dateFile, additionalFields);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,
+                    $"OptionAdditionalFieldGenerator.Run(): Error processing addtional fields for date {_processingDate:yyyy-MM-dd}");
+                return false;
+            }
+            return true;
+        }
+
+        private List<decimal> GetIvs(DateTime currentDateTime, string path)
+        {
+            // get i-year ATM IVs to calculate IV rank and percentile
+            var lastYearFiles = Directory.EnumerateFiles(path, "*.csv")
+                .AsParallel()
+                .Where(file => DateTime.TryParseExact(Path.GetFileNameWithoutExtension(file), "yyyyMMdd", 
+                        CultureInfo.InvariantCulture, DateTimeStyles.None, out var fileDate)
+                    && fileDate > currentDateTime.AddYears(-1)
+                    && fileDate <= currentDateTime)
+                .OrderBy(file => file)
+                .ToList();
+
+            return lastYearFiles.Select(csvFile => GetAtmIv(currentDateTime, csvFile))
+                .ToList();
+        }
+
+        private decimal GetAtmIv(DateTime currentDateTime, string csvPath)
+        {
+            var lines = File.ReadAllLines(csvPath)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .ToList();
+            var headers = lines[0].Split(',');
+            var deltaIndex = Array.IndexOf(headers, _deltaHeader);
+            var ivIndex = Array.IndexOf(headers, _impliedVolHeader);
+            var sidIndex = Array.IndexOf(headers, _sidHeader);
+            var tickerIndex = Array.IndexOf(headers, _tickerHeader);
+
+            if (deltaIndex == -1 || ivIndex == -1 || sidIndex == -1 || tickerIndex == -1)
+            {
+                return -1m;
+            }
+
+            var filtered = lines.Skip(2)
+                .Select(line =>
+                {
+                    var values = line.Split(',');
+                    var symbol = new Symbol(SecurityIdentifier.Parse(values[sidIndex]), values[tickerIndex]);
+                    var delta = decimal.Parse(values[deltaIndex]);
+                    var iv = decimal.Parse(values[ivIndex]);
+                    return (Expiry: symbol.ID.Date, Delta: delta, ImpliedVolatility: iv);
+                })
+                .Where(x => x.Expiry >= currentDateTime.AddDays(30))
+                .ToList();
+            if (filtered.Count == 0)
+            {
+                return -1m;
+            }
+
+            // Skip underlying row
+            return filtered.OrderBy(x => x.Expiry)
+                .ThenBy(x => Math.Abs(x.Delta - 0.5m))
+                .First()
+                .ImpliedVolatility;
+        }
+    }
+}
